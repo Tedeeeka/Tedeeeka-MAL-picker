76435e0fb14bc9a05f3a9c8a5d65d6192a02aa57
Tedeeeka <mimitedeeka@gmail.com>
2026-07-13 23:24:36 -0400

    Delete android-app directory

--- android-app/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.malpicker.helper',
-  appName: 'MAL Picker Helper',
-  webDir: 'www'
-};
-
-export default config;
