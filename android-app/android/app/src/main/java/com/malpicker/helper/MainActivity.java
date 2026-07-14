ef8ff4f51dec5e5098df1c4b32cbeda13055faaa
Tedeeeka <mimitedeeka@gmail.com>
2026-07-14 00:34:53 -0400

    Add files via upload

---
+++ android-app/android/app/src/main/java/com/malpicker/helper/MainActivity.java
@@ -0,0 +1,5 @@
+package com.malpicker.helper;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ android-app/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.malpicker.helper',
+  appName: 'MAL Picker Helper',
+  webDir: 'www'
+};
+
+export default config;
